0f345ca207dbf920fabb0d9dc1249b0598c22aff
skokenes <skokenes@axisgroup.com>
2017-06-14 15:14:01 -0400

    106 code

--- paint.js
+++ paint.js
@@ -50,14 +50,14 @@ define([], function() {
 			var currentRow = qMatrix[row];
 			// Create a row
 			var tr = document.createElement("tr");
-			// Iterate through each column of the row
-			for(var col = 0; col < currentRow.length; col++) {
-				// Get current cell data
-				var currentCell = currentRow[col];
-				// Create a cell
-				var td = document.createElement("td");
-				// Add text value to the cell
-				td.innerHTML = currentCell.qText;
+            // Iterate through each column of the row
+            for(var col = 0; col < currentRow.length; col++) {
+                // Get current cell data
+                var currentCell = currentRow[col];
+                // Create a cell
+                var td = document.createElement("td");
+                // Add text value to the cell
+                td.innerHTML = currentCell.qText;
 				
 				// Check if dimension, then add metadata
 				if(col < dimensionInfo.length) {
