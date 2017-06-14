3308d1efed6b806a14816cfef9c1be3aaadd0e4d
skokenes <skokenes@axisgroup.com>
2017-06-14 13:36:49 -0400

    104 code

--- my-table.js
+++ my-table.js
@@ -39,6 +39,9 @@ define([], function() {
     };
 
     var myPaint = function($element, layout) {
+        // Backend API
+        var backendApi = this.backendApi;
+
         // Get the text color value
         var textColor = layout.textColor;
 
@@ -79,28 +82,50 @@ define([], function() {
         var qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
 
         // Iterate through each row of the qMatrix
-        for(var row = 0; row < qMatrix.length; row++) {
-            // Get current row data
-            var currentRow = qMatrix[row];
-            // Create a row
-            var tr = document.createElement("tr");
-            // Iterate through each column of the row
-            for(var col = 0; col < currentRow.length; col++) {
-                // Get current cell data
-                var currentCell = currentRow[col];
-                // Create a cell
-                var td = document.createElement("td");
-                // Add text value to the cell
-                td.innerHTML = currentCell.qText;
-                // Append the cell to the row
-                tr.appendChild(td);
-            }
-            // append the row to the table
-            table.appendChild(tr);
-        }
+		for(var row = 0; row < qMatrix.length; row++) {
+			// Get current row data
+			var currentRow = qMatrix[row];
+			// Create a row
+			var tr = document.createElement("tr");
+			// Iterate through each column of the row
+			for(var col = 0; col < currentRow.length; col++) {
+				// Get current cell data
+				var currentCell = currentRow[col];
+				// Create a cell
+				var td = document.createElement("td");
+				// Add text value to the cell
+				td.innerHTML = currentCell.qText;
+				
+				// Check if dimension, then add metadata
+				if(col < dimensionInfo.length) {
+                    // Add a selectable class
+					td.className = "selectable";
+					// Add metadata for the selection
+					td.setAttribute("dim-col",col);
+					td.setAttribute("dim-index", currentCell.qElemNumber);
+				}
+				
+				// Append the cell to the row
+				tr.appendChild(td);
+			}
+			// append the row to the table
+			table.appendChild(tr);
+		}
 
         // Append the table to the $element
         $element.append(table);
+
+        // Add click functions to ".selectable" items
+		$element.find(".selectable").on("click", function() {
+			// Get the dimension column number
+			var dimCol = parseInt(this.getAttribute("dim-col"));
+			
+			// Get the dimension value index
+			var dimInd = parseInt(this.getAttribute("dim-index"));
+
+			// Call selectValues with these values
+			backendApi.selectValues(dimCol, [dimInd],true);
+		});
     };
 
     var mySupport = {
