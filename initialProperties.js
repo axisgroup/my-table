3fc8d491207426d4d87b880f98decaa68c90be37
skokenes <skokenes@axisgroup.com>
2017-06-14 14:45:42 -0400

    105 code

---
+++ initialProperties.js
@@ -0,0 +1,17 @@
+define([], function() {
+	return {
+		textColor: "black",
+		qHyperCubeDef: {
+			qDimensions: [],
+			qMeasures: [],
+			qInitialDataFetch: [
+				{
+					qTop: 0,
+					qLeft: 0,
+					qWidth: 10,
+					qHeight: 1000
+				}
+			]
+		}
+	};
+});
